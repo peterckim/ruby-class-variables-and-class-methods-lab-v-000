c2eb92af5032497eef2f1952927881154b8f64ad
Peter Kim <peterckim96@gmail.com>
2018-06-08 02:55:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,8 +27,15 @@ class Song
   end
   
   def genre_count
+    genre_count = {}
     @@genres.each do |genre|
-      {genre => }
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
   
   def artist_count
